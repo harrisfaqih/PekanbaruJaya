d3c725510b7241ab4dff8ed4a3be39baa9242a82
harris faqih <qifahsirrah@gmail.com>
2024-08-20 12:36:47 +0700

    fixing shopProducts

--- frontend/src/components/products/ShopProducts.jsx
+++ frontend/src/components/products/ShopProducts.jsx
@@ -2,16 +2,64 @@ import React, { useState } from "react";
 import { FaEye, FaRegHeart } from "react-icons/fa";
 import { RiShoppingCartLine } from "react-icons/ri";
 import Rating from "../Rating";
+import { useDispatch, useSelector } from "react-redux";
+import { add_to_card, add_to_wishlist } from "../../store/reducers/cardReducer";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 
 const ShopProducts = ({ styles, products }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.auth);
+
   const [selectedSizes, setSelectedSizes] = useState(
-    products.map((p) => p.sizes[0]) // Inisialisasi dengan ukuran pertama untuk setiap produk
+    products.map((p) => p.sizes[0])
   );
 
   const handleSizeChange = (index, size) => {
     const newSizes = [...selectedSizes];
-    newSizes[index] = size; // Update ukuran yang dipilih untuk produk tertentu
+    newSizes[index] = size;
     setSelectedSizes(newSizes);
+  };
+
+  const add_wishlist = (product) => {
+    if (userInfo) {
+      dispatch(
+        add_to_wishlist({
+          userId: userInfo.id,
+          productId: product._id,
+          name: product.name,
+          price: product.price,
+          image: product.images[0],
+          discount: product.discount,
+          rating: product.rating,
+          slug: product.slug,
+          size: selectedSizes[products.size],
+        })
+      );
+    } else {
+      navigate("/login");
+    }
+  };
+
+  const add_card = (product) => {
+    const selectedSize = selectedSizes[products.indexOf(product)];
+    if (!selectedSize) {
+      toast.error("Please select a size.");
+      return;
+    }
+    if (userInfo) {
+      dispatch(
+        add_to_card({
+          userId: userInfo.id,
+          quantity: 1,
+          productId: product._id,
+          size: selectedSize,
+        })
+      );
+    } else {
+      navigate("/login");
+    }
   };
 
   return (
@@ -45,13 +93,23 @@ const ShopProducts = ({ styles, products }) => {
             />
 
             <ul className="flex transition-all duration-700 -bottom-10 justify-center items-center gap-2 absolute w-full group-hover:bottom-3">
-              <li className="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all">
+              <li
+                onClick={() => add_wishlist(p)}
+                className="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all"
+              >
                 <FaRegHeart />
               </li>
-              <li className="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all">
+
+              <Link
+                to={`/product/details/${p.slug}`}
+                className="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all"
+              >
                 <FaEye />
-              </li>
-              <li className="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all">
+              </Link>
+              <li
+                onClick={() => add_card(p)}
+                className="w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all"
+              >
                 <RiShoppingCartLine />
               </li>
             </ul>
@@ -64,10 +122,9 @@ const ShopProducts = ({ styles, products }) => {
                 <Rating ratings={p.rating} />
               </div>
             </div>
-            {/* Dropdown untuk memilih ukuran */}
             <select
-              value={selectedSizes[i]} // Menggunakan ukuran yang dipilih untuk produk ini
-              onChange={(e) => handleSizeChange(i, e.target.value)} // Memperbarui ukuran yang dipilih
+              value={selectedSizes[i]}
+              onChange={(e) => handleSizeChange(i, e.target.value)}
               className="mt-2 p-1 border rounded"
             >
               {p.sizes.map((size) => (

4c7652b05c4b8ee4efbf0602e8a9fac00e3e50d8
harris faqih <qifahsirrah@gmail.com>
2024-08-26 02:36:11 +0700

    fixing stock2

--- dashboard/src/views/admin/DiscountProducts.jsx
+++ dashboard/src/views/admin/DiscountProducts.jsx
@@ -1,19 +1,42 @@
 /* eslint-disable jsx-a11y/scope */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "../components/Search";
 import { Link } from "react-router-dom";
 import Pagination from "../Pagination";
 import { FaEdit, FaEye, FaTrash } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  get_products,
+  delete_product,
+} from "../../store/Reducers/productReducers";
 
 const DiscountProducts = () => {
+  const dispatch = useDispatch();
+  const { products, totalProduct } = useSelector((state) => state.product);
+
   const [currentPage, setCurrentPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
   const [parPage, setParPage] = useState(5);
 
+  const handleDelete = (id) => {
+    if (window.confirm("Apakah Anda yakin ingin menghapus produk ini?")) {
+      dispatch(delete_product(id));
+    }
+  };
+
+  useEffect(() => {
+    const obj = {
+      parPage: parseInt(parPage),
+      page: parseInt(currentPage),
+      searchValue,
+    };
+    dispatch(get_products(obj));
+  }, [searchValue, currentPage, parPage]);
+
   return (
     <div className="px-2 lg:px-7 pt-5">
       <h1 className="text-[#000000] font-semibold text-lg mb-3">
-        Discount Products
+        Stock Monitoring
       </h1>
 
       <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
@@ -58,105 +81,102 @@ const DiscountProducts = () => {
             </thead>
 
             <tbody>
-              {
-                // eslint-disable-next-line react/jsx-no-comment-textnodes
-                [1, 2, 3, 4, 5].map((d, i) => (
-                  <tr key={i}>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      {d}
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      <img
-                        className="w-[45px] h-[45px]"
-                        src={`http://localhost:3000/images/${d}.jpg`}
-                        alt=""
-                      />
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      {
-                        [
-                          "gembok ats 30",
-                          "gembok vion 30",
-                          "gembok xp tool 30",
-                          "gerinda ats",
-                          "gerinda maktech",
-                        ][i]
-                      }
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      Tools
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      {["ATS", "VIAN", "XP TOOL", "ATS", "MAKTECH"][i]}
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      $232
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      10%
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      20
-                    </td>
-                    <td
-                      scope="row"
-                      className="py-1 px-4 font-medium whitespace-nowrap"
-                    >
-                      <div className="flex justify-start items-center gap-4">
-                        <Link className="p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50">
-                          {" "}
-                          <FaEdit />{" "}
-                        </Link>
-                        <Link className="p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50">
-                          {" "}
-                          <FaEye />{" "}
-                        </Link>
-                        <Link className="p-[6px] bg-red-500 rounded hover:shadow-lg hover:shadow-red-500/50">
-                          {" "}
-                          <FaTrash />{" "}
-                        </Link>
-                      </div>
-                    </td>
-                  </tr>
-                ))
-              }
+              {products.map((d, i) => (
+                <tr key={i}>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    {i + 1}
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    <img
+                      className="w-[45px] h-[45px]"
+                      src={d.images[0]}
+                      alt=""
+                    />
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    {d?.name?.slice(0, 15)}...
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    {d.category}
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    {d.brand}
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    Rp{d.price}
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    {d.discount === 0 ? (
+                      <span>No Discount</span>
+                    ) : (
+                      <span>%{d.discount}</span>
+                    )}
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    {d.stock}
+                  </td>
+                  <td
+                    scope="row"
+                    className="py-1 px-4 font-medium whitespace-nowrap"
+                  >
+                    <div className="flex justify-start items-center gap-4">
+                      <Link
+                        to={`/admin/dashboard/edit-product/${d._id}`}
+                        className="p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500/50"
+                      >
+                        <FaEdit />
+                      </Link>
+                      <button
+                        onClick={() => handleDelete(d._id)}
+                        className="p-[6px] bg-red-500 rounded hover:shadow-lg hover:shadow-red-500/50"
+                      >
+                        <FaTrash />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
 
-        <div className="w-full flex justify-end mt-4 bottom-4 right-4">
-          <Pagination
-            pageNumber={currentPage}
-            setPageNumber={setCurrentPage}
-            totalItem={50}
-            parPage={parPage}
-            showItem={3}
-          />
-        </div>
+        {totalProduct <= parPage ? (
+          ""
+        ) : (
+          <div className="w-full flex justify-end mt-4 bottom-4 right-4">
+            <Pagination
+              pageNumber={currentPage}
+              setPageNumber={setCurrentPage}
+              totalItem={200}
+              parPage={parPage}
+              showItem={5}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
